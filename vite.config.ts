1f70358bc5d6128bc08a085baed5a9bb34d0afb8
aghilpadash <aghilapdash73@gmail.com>
2026-06-25 01:49:20 +0330

    Change base path to root '/' for main domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Everest-Website/', // Use relative paths for static assets on GH Pages
+  base: '/', // Use relative paths for static assets on GH Pages
 })
